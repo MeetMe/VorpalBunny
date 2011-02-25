3641d76d66d12bbb5952ab2697db8b0374dbe6d0
Gavin M. Roy <gmr@myyearbook.com>
2011-02-24 19:53:51 -0500

    Command line test app

---
+++ test.php
@@ -0,0 +1,23 @@
+<?php
+require_once("vorpalbunny.php");
+
+// Allow a command line override of localhost
+if ( strlen($argv) > 1 )
+{
+  $broker = $argv[1];
+} else {
+  $broker = 'localhost';
+}
+
+// Create our VorpalBunny object
+$vb = new VorpalBunny( $broker );
+
+// Publish to our rabbitmq broker
+if ( $vb->publish( "Hello World!", "", "test" ) )
+{
+  print "Message Published\n";
+}
+else
+{
+  print "Error delivering message.\n";
+}
